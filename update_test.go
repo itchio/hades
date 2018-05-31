47671728a05d66fa6ba505d57bd4552b7ca42ab2
Amos Wenger <amos@itch.io>
2018-05-31 22:55:02 +0200

    Add Context.Update

---
+++ update_test.go
@@ -0,0 +1,69 @@
+package hades_test
+
+import (
+	"testing"
+
+	"crawshaw.io/sqlite"
+	"github.com/go-xorm/builder"
+	"github.com/itchio/hades"
+	"github.com/itchio/wharf/wtest"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Update(t *testing.T) {
+	type Mistake struct {
+		ID   int64
+		Body string
+	}
+
+	models := []interface{}{&Mistake{}}
+
+	withContext(t, models, func(conn *sqlite.Conn, c *hades.Context) {
+		mistakes := []*Mistake{
+			&Mistake{
+				ID:   1,
+				Body: "rewrote everything",
+			},
+			&Mistake{
+				ID:   2,
+				Body: "mostly alone",
+			},
+		}
+
+		var count int64
+		var err error
+
+		wtest.Must(t, c.SaveOne(conn, mistakes))
+
+		count, err = c.Count(conn, &Mistake{}, builder.NewCond())
+		wtest.Must(t, err)
+		assert.EqualValues(t, 2, count)
+
+		wtest.Must(t, c.Update(conn, &Mistake{},
+			hades.Where(builder.Eq{"id": 1}),
+			builder.Eq{"body": "rewrote almost everything"},
+		))
+
+		var m Mistake
+		var found bool
+		found, err = c.SelectOne(conn, &m, builder.Eq{"id": 1})
+		wtest.Must(t, err)
+		assert.True(t, found)
+		assert.EqualValues(t, "rewrote almost everything", m.Body)
+
+		wtest.Must(t, c.Update(conn, &Mistake{},
+			hades.Where(builder.Expr("1")),
+			builder.Eq{"body": "nothing"},
+		))
+
+		found, err = c.SelectOne(conn, &m, builder.Eq{"id": 1})
+		wtest.Must(t, err)
+		assert.True(t, found)
+		assert.EqualValues(t, "nothing", m.Body)
+
+		found, err = c.SelectOne(conn, &m, builder.Eq{"id": 2})
+		wtest.Must(t, err)
+		assert.True(t, found)
+		assert.EqualValues(t, "nothing", m.Body)
+	})
+}
